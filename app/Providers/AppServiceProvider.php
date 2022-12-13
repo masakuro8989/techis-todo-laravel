a3c3e98fcced8bce544e6a5b6b0b6e4d8cb8e2d2
96neko3213kyd@gmail.com <96neko3213kyd@gmail.com>
2022-12-13 23:03:09 +0900

    aa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        
         //
+        if(\App::environment(['production'])){
+            \URL::forceSchme('https');
+        }
     }
 }
